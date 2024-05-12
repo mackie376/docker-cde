51d1dea14a9154c299e599568a568ff28fc771d6
Takashi Makimoto <mackie@beehive-dev.com>
2024-05-12 17:31:37 +0900

    neo-tree を有効にした

--- conf.d/nvim/lua/plugins/editor.lua
+++ conf.d/nvim/lua/plugins/editor.lua
@@ -16,7 +16,42 @@ end
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    enabled = false,
+    opts = {
+      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+      open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+      },
+      window = {
+        mappings = {
+          ["<space>"] = "none",
+          ["Y"] = {
+            function(state)
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              vim.fn.setreg("+", path, "c")
+            end,
+            desc = "Copy Path to Clipboard",
+          },
+          ["O"] = {
+            function(state)
+              require("lazy.util").open(state.tree:get_node().path, { system = true })
+            end,
+            desc = "Open with System Application",
+          },
+        },
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
+        },
+      },
+    },
   },
   {
     "echasnovski/mini.hipatterns",
